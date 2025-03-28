4159b57e90fe69db6b74ea48e95cf475909061aa
Rajath Kiran A <arajath5463@gmail.com>
2025-03-28 19:35:57 +0530

    Trying to add the pre designed hero section

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 // App.js
 import React from "react";
 import Navbar from './components/Navbar';
+import Hero from './components/Hero';
 
 const App = () => {
   return (
     <div>
       <Navbar />
       {/* Rest of the app */}
+      <Hero />
     </div>
   );
 };
