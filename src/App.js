3e56639119aab9c4d1b48d436de2c2ce72127774
Rajath Kiran A <arajath5463@gmail.com>
2025-03-29 20:42:30 +0530

    Adde Footer Component from react template

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
-// App.js
 import React from "react";
-import Navbar from './components/Navbar';
-
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 
 const App = () => {
   return (
-    <div>
+    <div className="flex flex-col min-h-screen">
       <Navbar />
-      {/* Rest of the app */}
- 
+      
+      {/* Empty section space */}
+      <div className="h-64 bg-gray-200 my-10"></div> 
+
+      <h1 className="text-center text-xl font-bold">Welcome to My Website</h1>
+      
+      {/* Other page content */}
+
+      <Footer />
     </div>
   );
 };
