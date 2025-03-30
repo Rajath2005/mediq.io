3e6335fa595253255cae8f2a236ca90c5e3a0bb3
Rajath Kiran A <arajath5463@gmail.com>
2025-03-30 19:14:51 +0530

    Deleted the Navbar component to Fix the Problem of Navbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import React from "react";
-import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 
 const App = () => {
   return (
     <div>
-      <Navbar />
       <Footer />
     </div>
   );
