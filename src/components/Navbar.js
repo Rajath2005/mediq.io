972c80f7778567f929b9ff707488254893196890
Rajath Kiran A <arajath5463@gmail.com>
2025-03-31 10:42:48 +0530

    Some errors in the Navbar.js is fixed

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,6 +7,7 @@ const Navbar = () => {
   const [darkMode, setDarkMode] = useState(
     localStorage.getItem("darkMode") === "true"
   );
+  const [isNavExpanded, setIsNavExpanded] = useState(false);
 
   useEffect(() => {
     if (darkMode) {
@@ -22,7 +23,7 @@ const Navbar = () => {
       <div className="container-fluid">
         
         {/* Logo & Brand */}
-        <a className="navbar-brand d-flex align-items-center" href="#">
+        <a className="navbar-brand d-flex align-items-center" href="/">
           <img src={logo} alt="Logo" className="navbar-logo me-2" />
           Navbar
         </a>
@@ -31,39 +32,36 @@ const Navbar = () => {
         <button
           className="navbar-toggler"
           type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarSupportedContent"
+          onClick={() => setIsNavExpanded(!isNavExpanded)}
           aria-controls="navbarSupportedContent"
-          aria-expanded="false"
+          aria-expanded={isNavExpanded}
           aria-label="Toggle navigation"
         >
           <span className="navbar-toggler-icon"></span>
         </button>
 
         {/* Navbar Links */}
-        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+        <div className={`collapse navbar-collapse ${isNavExpanded ? 'show' : ''}`} id="navbarSupportedContent">
           <ul className="navbar-nav me-auto mb-2 mb-lg-0">
             <li className="nav-item">
-              <a className="nav-link active" href="#">Home</a>
+              <a className="nav-link active" href="/">Home</a>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="#">Link</a>
+              <a className="nav-link" href="/link">Link</a>
             </li>
             <li className="nav-item dropdown">
-              <a
+              <button
                 className="nav-link dropdown-toggle"
-                href="#"
-                role="button"
                 data-bs-toggle="dropdown"
                 aria-expanded="false"
               >
                 Dropdown
-              </a>
+              </button>
               <ul className="dropdown-menu">
-                <li><a className="dropdown-item" href="#">Action</a></li>
-                <li><a className="dropdown-item" href="#">Another action</a></li>
+                <li><a className="dropdown-item" href="/action">Action</a></li>
+                <li><a className="dropdown-item" href="/another-action">Another action</a></li>
                 <li><hr className="dropdown-divider" /></li>
-                <li><a className="dropdown-item" href="#">Something else here</a></li>
+                <li><a className="dropdown-item" href="/something-else">Something else here</a></li>
               </ul>
             </li>
           </ul>
