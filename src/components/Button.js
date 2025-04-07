f143dcdfd6820e04893ae8d313c10ae545098c9d
Rajath Kiran A <arajath5463@gmail.com>
2025-04-07 20:02:28 +0530

    updated

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,10 +1,19 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './Button.css'; // Import the external CSS file
 
-const Button = ({ className, text }) => {
+const Button = ({ className, text, to, ...props }) => {
   return (
     <div className={`styled-wrapper ${className}`}>
-      <button>{text}</button>
+      {to ? (
+        <Link to={to} className={`button ${className}`} {...props}>
+          {text}
+        </Link>
+      ) : (
+        <button className={`button ${className}`} {...props}>
+          {text}
+        </button>
+      )}
     </div>
   );
 };
