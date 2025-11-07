24e836709af5738f8da4025e829d42e8efca75fe
Rajath Kiran A <arajath5463@gmail.com>
2025-11-07 20:12:51 +0530

    Moved Firebase config to env file and secured secrets

--- src/firebase.js
+++ src/firebase.js
@@ -1,24 +1,21 @@
 // firebase.js
-import { initializeApp } from 'firebase/app';
-import { getAuth } from 'firebase/auth';
-import { getFirestore } from 'firebase/firestore';
-import { getStorage } from 'firebase/storage';
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCz-Cump08AEM6rpVHKKiXs1c0CHGBBneA",
-  authDomain: "mediq-e53c7.firebaseapp.com",
-  projectId: "mediq-e53c7",
-  storageBucket: "mediq-e53c7.firebasestorage.app",
-  messagingSenderId: "627980791325",
-  appId: "1:627980791325:web:90b4d632119c9574d5e0f4",
-  measurementId: "G-LG1PNVNH3E"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
-// Initialize Firebase services
 export const auth = getAuth(app);
 export const db = getFirestore(app);
 export const storage = getStorage(app);
